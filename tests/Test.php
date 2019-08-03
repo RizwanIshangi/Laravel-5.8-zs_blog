8a7d53591dfae5e547615b0ff56e0bcfcec6c7b5
Rizwan <rizwan.ishangi@gmail.com>
2019-08-03 14:46:15 +0500

    first commit rizwan

---
+++ tests/Test.php
@@ -0,0 +1,10 @@
+<?php
+echo "string";
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
